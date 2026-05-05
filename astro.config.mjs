c176281a505c9641f6bc7288e7206ffeade98fb1
“MuhammadKurniaSani-me” <sani230402@gmail.com>
2026-05-06 00:14:27 +0700

    fix: add site and base path to astro config for github pages styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    // Your main GitHub Pages domain
+    site: "https://MuhammadKurniaSani-me.github.io",
+
+    // Your exact repository name starting with a forward slash
+    base: "/pricing-bag",
 });
